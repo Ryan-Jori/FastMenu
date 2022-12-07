15d819bfb84eb5ab591bb46bbb0985948089dc46
Ryan-Jori <79109999+Ryan-Jori@users.noreply.github.com>
2022-12-06 22:33:39 -0300

    Add files via upload

---
+++ main/java/com/example/restaurantapp/Pedido.java
@@ -0,0 +1,16 @@
+package com.example.restaurantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pedido);
+    }
+
+
+}
